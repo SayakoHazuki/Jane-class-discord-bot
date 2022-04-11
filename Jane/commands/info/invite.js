80ef529c6c17ca2d14d21eae5f28c139fb7c929a
Kai Kamf <74895715+Kai9073@users.noreply.github.com>
2022-04-11 22:18:18 +0800

    邀請連結指令中以按鈕取代原本的超連結

--- Jane/commands/info/invite.js
+++ Jane/commands/info/invite.js
@@ -15,12 +15,14 @@ module.exports = class InviteCommand extends Command {
   }
 
   async run (message, args) {
-    const inviteEmbed = new Discord.MessageEmbed()
-      .setTitle('簡 - 邀請連結')
-      .setDescription(
-        '[按此把簡加進其他伺服器](https://discord.com/api/oauth2/authorize?client_id=801354940265922608&permissions=8&redirect_uri=https%3A%2F%2Fjanesite.ga%2F&scope=bot%20applications.commands)'
-      )
-      .setColor(this.client.colors.blue)
-    message.reply({ embeds: [inviteEmbed] })
+    const linkButtons = new Discord.MessageActionRow().addComponents([
+      new Discord.MessageButton()
+        .setStyle('LINK')
+        .setLabel('把簡加進其他伺服器')
+        .setURL(
+          'https://discord.com/api/oauth2/authorize?client_id=801354940265922608&permissions=8&scope=bot'
+        )
+    ])
+    message.reply({ components: linkButtons })
   }
 }
