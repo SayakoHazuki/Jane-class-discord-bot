6ecb59aa32e8db56795eef3fec9b9c5a42a532cb
Kai Kamf <74895715+Kai9073@users.noreply.github.com>
2022-04-11 22:14:55 +0800

    校歷表指令中以按鈕取代原本的超連結

--- Jane/commands/info/calendar.js
+++ Jane/commands/info/calendar.js
@@ -17,30 +17,31 @@ module.exports = class CalendarCommand extends Command {
 
   async run (message, args) {
     try {
-      const calEmbedTitle = new Discord.MessageEmbed()
-        .setTitle('21/22年度校曆表')
-        .setFooter(
-          `${message.author.tag} 使用了 -cal`,
-          message.author.displayAvatarURL()
-        )
-        .setColor(this.client.themeColor)
-        .setDescription(
-          '校歷表(PDF檔案) > https://jane.ml/files/calendar.pdf\n校歷表(PNG圖片) > https://jane.ml/academic/cal'
-        )
       const calEmbed = new Discord.MessageEmbed()
+        .setTitle('21/22年度校曆表')
         .setImage('https://jane.ml/files/cal_2.png')
         .setColor(this.client.themeColor)
-      message.channel
-        .createWebhook('簡 - 校歷表', {
-          avatar: this.client.user.avatarURL({ format: 'png' })
+        .setFooter({
+          text: `${message.author.tag} 使用了 -cal`,
+          iconURL: message.author.displayAvatarURL()
         })
-        .then(w => {
-          w.send({
-            embeds: [calEmbedTitle, calEmbed]
-          })
-          setTimeout(function () {
-            w.delete()
-          }, 2500)
+      const linkButtons = new Discord.MessageActionRow().addComponents([
+        new Discord.MessageButton()
+          .setStyle('LINK')
+          .setLabel('下載 (PDF)')
+          .setURL('https://jane.ml/files/calendar.pdf'),
+        new Discord.MessageButton()
+          .setStyle('LINK')
+          .setLabel('下載 (PNG)')
+          .setURL('https://jane.ml/academic/cal')
+      ])
+      message
+        .reply({
+          embeds: [calEmbed],
+          components: [linkButtons]
+        })
+        .catch(e => {
+          Util.printLog('ERR', __filename, e.stack)
         })
     } catch (e) {
       Util.printLog('err', __filename, e.message)
